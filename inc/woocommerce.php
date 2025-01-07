646a58d41674890e0767f0494bc8ce1315478eec
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2025-01-07 15:31:52 -0500

    add: product search

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -3,16 +3,15 @@
 class WC_Custom {
   function __construct() {
     add_action('woocommerce_single_product_summary', array($this, 'add_ws_button'), 25);
-    add_action('get_search_form', array($this, 'add_post_type_to_search_form'));
+    add_action('pre_get_posts', array($this, 'force_search_to_products'));
   }
 
-  function add_post_type_to_search_form($form) {
-    $form = str_replace(
-      '</form>',
-      '<input type="hidden" name="post_type" value="product" /></form>',
-      $form
-    );
-    return $form;
+  function force_search_to_products($query) {
+    if ($query->is_search() && !is_admin() && $query->is_main_query()) {
+      if (!isset($_GET['post_type']) || $_GET['post_type'] !== 'product') {
+        $query->set('post_type', 'product');
+      }
+    }
   }
 }
 
