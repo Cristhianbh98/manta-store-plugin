05f6ddcf36f2bc4ca0a095cda36dff24d740b1dc
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2025-01-07 15:40:11 -0500

    add: redireciton to post rtype product

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -3,13 +3,24 @@
 class WC_Custom {
   function __construct() {
     add_action('woocommerce_single_product_summary', array($this, 'add_ws_button'), 25);
-    add_action('pre_get_posts', array($this, 'force_search_to_products'));
+    add_action('template_redirect', array($this, 'redirect_search_if_no_post_type'));
   }
 
-  function force_search_to_products($query) {
-    if ($query->is_search() && !is_admin() && $query->is_main_query()) {
+  function redirect_search_if_no_post_type() {
+    if (is_search() && !is_admin()) {
       if (!isset($_GET['post_type']) || $_GET['post_type'] !== 'product') {
-        $query->set('post_type', 'product');
+        $search_query = get_query_var('s');
+        
+        $redirect_url = add_query_arg(
+          array(
+            's' => urlencode($search_query),
+            'post_type' => 'product'
+          ),
+          home_url('/')
+        );
+
+        wp_redirect($redirect_url);
+        exit;
       }
     }
   }
