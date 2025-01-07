4f252eed1d36a4f0fcfc38fc14cbce4200f957f0
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2025-01-07 11:52:51 -0500

    change: marca category needed

---
+++ inc/woocommerce.php
@@ -0,0 +1,50 @@
+<?php
+
+class WC_Custom {
+  function __construct() {
+    add_action('woocommerce_single_product_summary', array($this, 'add_ws_button'), 25);
+    add_filter('woocommerce_is_purchasable', '__return_false');
+  }
+
+  function add_ws_button() {
+    global $product;
+    
+    if ($product) {
+      $categories = wp_get_post_terms($product->get_id(), 'product_cat');
+      $phone_number = '';
+  
+      if (!empty($categories) && !is_wp_error($categories)) {
+        $brand_category = null;
+
+        foreach($categories as $category) {
+          if ($category->parent > 0) {
+            $parent_category = get_term($category->parent, 'product_cat');
+
+            if ($parent_category && $parent_category->slug === 'marca') {
+              $brand_category = $category;
+              break;
+            }
+          }
+        }
+
+        if ($brand_category) {
+          $phone_number = get_term_meta($brand_category->term_id, 'phone_number', true);
+
+          $product_name = $product->get_name();
+          $product_price = $product->get_price();
+      
+          $ws_uri = 'https://api.whatsapp.com/send?phone=' . $phone_number . '&text=¡Hola!%20Estoy%20interesado%20en%20el%20producto%20' . $product_name . '%20que%20tiene%20un%20precio%20de%20' . $product_price . '%20dólares.';
+      
+          echo '<a href="' . $ws_uri . '" class="ws-button" target="_blank">
+                  <button>
+                    <i class="fab fa-whatsapp"></i>
+                    ¡Lo quiero!
+                  </button>
+                </a>';
+        }
+      } 
+    }
+  }  
+}
+
+new WC_Custom();
