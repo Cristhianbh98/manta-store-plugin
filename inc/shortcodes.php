a709e4c379e24331948713239bf64f8b4035da03
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2025-02-06 07:24:03 -0500

    add: global ws

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -8,7 +8,7 @@ class Shortcodes {
   function ws_button() {
     global $product;
     
-    if ($product) {
+    /* if ($product) {
       $categories = wp_get_post_terms($product->get_id(), 'product_cat');
       $phone_number = '';
   
@@ -42,9 +42,18 @@ class Shortcodes {
                 </a>';
         }
       } 
-    }
+    } */
 
-    return '';
+    $product_name = $product->get_name();
+    $product_price = $product->get_price(); 
+    $ws_uri = 'https://api.whatsapp.com/send?phone=593981516241&text=¡Hola!%20Estoy%20interesado%20en%20el%20producto%20' . $product_name . '%20que%20tiene%20un%20precio%20de%20' . $product_price . '%20dólares.';
+
+    return '<a href="' . $ws_uri . '" class="ws-button" target="_blank">
+              <button>
+                <i class="fab fa-whatsapp"></i>
+                ¡Lo quiero!
+              </button>
+            </a>';
   }  
 }
 
